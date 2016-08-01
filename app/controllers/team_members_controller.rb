844471306b11cbe9467fa35d9895866d22c44b45
gnfisher <me@gnfisher.com>
2016-08-01 11:21:39 -0400

    add new action and view to team_members

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,9 +1,16 @@
 class TeamMembersController < ApplicationController
-  before_action :set_project, only: [:index]
+  before_action :set_project, only: [:index, :new]
 
   def index
     @team_members = @project.team_members.includes(:user)
     authorize @project, :view_team_members?
+  end
+
+  def new
+    @users = User.not_on_team(@project).reject do |user|
+      @project.owner == user
+    end
+    authorize @project, :add_team_members?
   end
 
   private
