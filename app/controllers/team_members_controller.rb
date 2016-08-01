cc02b5f2144b302b153fc7a6aa3c038c6f834ecf
gnfisher <me@gnfisher.com>
2016-08-01 12:34:58 -0400

    add create action for multiple team members

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,5 +1,5 @@
 class TeamMembersController < ApplicationController
-  before_action :set_project, only: [:index, :new]
+  before_action :set_project, only: [:index, :new, :create]
 
   def index
     @team_members = @project.team_members.includes(:user)
@@ -13,9 +13,26 @@ class TeamMembersController < ApplicationController
     authorize @project, :add_team_members?
   end
 
+  def create
+    authorize @project, :add_team_members?
+    if TeamMember.create_multiple(team_members_params[:users])
+      redirect_to project_team_members_path(@project), notice: 'Permissions updated!'
+    else
+      flash[:alert] = "There was an issue updating user permissions. Try again."
+      @users = User.not_on_team(@project).reject do |user|
+        @project.owner == user
+      end
+      render :new
+    end
+  end
+
   private
 
   def set_project
     @project = Project.find_by(id: params[:project_id])
+  end
+
+  def team_members_params
+    params.permit(users: [:role, :project_id, :user_id])
   end
 end
--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,6 +1,22 @@
 class TeamMember < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
-  
+
   enum :role => [:view, :edit]
+
+  def self.create_multiple(team_members_attrs)
+    begin
+      TeamMember.transaction do
+        team_members_attrs.each do |k,team_member|
+          unless team_member[:role] == 'none'
+            user = self.create!(user_id: team_member[:user_id], project_id: team_member[:project_id])
+            user.send("#{team_member[:role]}!")
+          end
+        end
+      end
+      true
+    rescue
+      false
+    end
+  end
 end
